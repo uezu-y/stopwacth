f402eeb42ccc1b4750e5e44fddbd4efcbe8c452a
EC2 Default User <ec2-user@ip-172-31-5-229.ap-northeast-1.compute.internal>
2022-10-05 05:29:19 +0000

    ストップウォッチ

---
+++ stmain.js
@@ -0,0 +1,65 @@
+var startBtn = document.getElementById("start");
+var stopBtn = document.getElementById("stop"); 
+var resetBtn = document.getElementById("reset");
+var dispTime = document.getElementById("time");      
+
+var timer;              
+var startTime;          
+var elapsedTime = 0;    
+var stopTime = 0;       
+
+
+// スタートボタン押した時
+function start(){
+    
+    startTime = Date.now();
+
+    measure();
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    resetBtn.disabled = false;
+}
+
+// ストップボタン押した時
+function stop(){
+    
+    clearInterval(timer);
+
+    stopTime += Date.now() - startTime;
+
+    start.disabled = false;
+    stopBtn.disabled = true;
+    resetBtn.disabled = false;
+}
+
+// リセットボタン押した時
+function reset(){
+    
+    clearInterval(timer);
+
+    elapsedTime = 0;
+    stopTime = 0;
+    dispTime.textContent = "0:0:0:0";
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    resetBtn.disabled = true;
+}
+
+// 時間を計測する
+function measure() {
+    
+    timer = setTimeout(function () {
+        
+        // 経過時間を設定して、画面へ表示する
+        elapsedTime = Date.now() - startTime + stopTime;
+        dispTime.textContent = new Date(elapsedTime).toISOString().replace(/[^0-9]/g, '').slice(11, 15)
+        .replace(/(\d{1})(\d{1})(\d{1})(\d{1})/, "$1:$2:$3:$4");
+
+        measure();
+    }, 10);
+}
+
+
+
